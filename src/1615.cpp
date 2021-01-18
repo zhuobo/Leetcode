57260fa6c33b8881d22711d2e7b57b66d60ea840
tanzhuobo <tanzhuobo@gmail.com>
2021-01-18 20:32:37 +0800

    modify 1615

--- src/1615.cpp
+++ src/1615.cpp
@@ -1,10 +1,12 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
+#include <unordered_set>
 
 using namespace std;
 
 // Solution 1: brute force just beat 5.22%
+//
 class Solution {
 public:
     int maximalNetworkRank(int n, vector<vector<int>>& roads) {
@@ -34,6 +36,20 @@ public:
 class Solution2 {
 public:
     int maximalNetworkRank(int n, vector<vector<int>>& roads) {
-
+        vector<unordered_set<int>> adjacent(n, unordered_set<int>());
+        for ( const auto& road : roads ) {
+            adjacent[road[0]].insert(road[1]);
+            adjacent[road[1]].insert(road[0]);
+        }
+        int count = 0;
+        for ( int i = 0; i < n - 1; ++i ) {
+            for ( int j = i + 1; j < n; ++j ) {
+                count = max(count, adjacent[i].size() + adjacent[j].size());
+                if ( adjacent[i].find(j) != adjacent[i].end() ) {
+                    --count;
+                }
+            }
+        }
+        return count;
     }
 };
