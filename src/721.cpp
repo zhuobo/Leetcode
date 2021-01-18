084a95e4701f9c45496e44a84be391507a1ce112
tanzhuobo <tanzhuobo@gmail.com>
2021-01-18 11:30:45 +0800

    add 721

---
+++ src/721.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+class Solution {
+public:
+    vector<vector<string>> accountsMerge(vector<vector<string>>& accounts) {
+        
+    }
+};
