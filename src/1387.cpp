3c5ab29257a40a82a0484f44839f2c123d52fb29
tanzhuobo <tanzhuobo@gmail.com>
2021-01-18 17:04:22 +0800

    add solution 2 to 1387

--- src/1387.cpp
+++ src/1387.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
+#include <unordered_map>
 
 using namespace std;
 
@@ -35,5 +36,40 @@ public:
         }
         sort(nums.begin(), nums.end(), step());
             return nums[k - 1];
+    }
+};
+
+
+// Solution 2 using memo to reduce the calc
+class Solution2 {
+public:
+    int calc_steps(std::unordered_map<int, int>& steps, int x) {
+        if ( steps.find(x) != steps.end() ) {
+            return steps[x];
+        }
+        if ( x == 1 ) {
+            return steps[x] = 0;
+        }
+        if ( (x & 1) == 1 ) {
+                return steps[x] = calc_steps(steps, 3 * x + 1) + 1;
+        } else {
+            return steps[x] = calc_steps(steps, x / 2) + 1;
+        }
+    }
+
+    int getKth(int lo, int hi, int k) {
+        vector<int> nums;
+        unordered_map<int, int> steps;
+        for ( int i = lo; i <= hi; ++i ) {
+            nums.push_back(i);
+        }
+        sort(nums.begin(), nums.end(), [&](int a, int b){
+            if ( calc_steps(steps, a) != calc_steps(steps, b)  ) {
+                return calc_steps(steps, a) < calc_steps(steps, b);
+            } else {
+                return a < b;
+            }
+        });
+        return nums[k - 1];
     }
 };
