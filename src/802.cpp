039eb3344d830ce7414c0390a54020efa86167a0
tanzhuobo <tanzhuobo@gmail.com>
2021-01-19 17:54:19 +0800

    add solution 1 of 802: dfs

--- src/802.cpp
+++ src/802.cpp
@@ -6,34 +6,38 @@ using namespace std;
 
 class Solution {
 private:
-    bool dfs(vector<vector<int>>& graph, unordered_set<int>& safe_node, int node) {
-        if ( safe_node.find(node) != safe_node.end() ) {
+    bool dfs(vector<vector<int>>& graph, vector<int>& visited, int node) {
+        // 0: not visited; 1: visiting; 2: visited
+        if ( visited[node] == 2 ) {
             return true;
+        } else if ( visited[node] == 1 ) {
+            return false;
+        } else {
+            // 将要访问当前节点，将当前节点置为正在访问状态
+            // 如果在这一轮访问中，又访问到了这种正在访问状态的节点，
+            // 那么就能说明存在环
+            visited[node] = 1;
+            for ( const auto& next_node : graph[node] ) {
+                if ( !dfs(graph, visited, next_node) ) {
+                    return false;
+                } }
+            // 否则，这个节点不会进入某个环中
+            visited[node] = 2;
         }
-        for ( int next_node : graph[node] ) {
-            if ( dfs(graph, safe_node, next_node) ) {
-                return true;
-            }
-        }
-        return false;
+        return true;
     }
 public:
     vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
         // graph is a adjacency list
         int n = graph.size();
-        unordered_set<int> safe_node;
+        vector<int> safe_nodes;
+        vector<int> visited(n, 0);
         for ( int i = 0; i < n; ++i ) {
-            if ( graph[i].empty() ) {
-                safe_node.insert(i);
+            if ( dfs(graph, visited, i) ) {
+                safe_nodes.push_back(i);
             }
+            visited.resize(n, 0);
         }
-        
-        vector<int> res;
-        for ( int i = 0; i < n; ++i ) {
-            if ( dfs(graph, safe_node, i) ) {
-                res.push_back(i);
-            }
-        }
-        return res;
+        return safe_nodes;
     }
 };
