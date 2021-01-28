2d11175bd9e72d313314db0edeab3a74a754fa59
tanzhuobo <tanzhuobo@gmail.com>
2021-01-28 15:21:51 +0800

    modified union_find

--- src/union_find.cpp
+++ src/union_find.cpp
@@ -7,9 +7,10 @@ class UnionFind {
 private:
     vector<int> parent;
     vector<int> rank;
+    vector<int> size;
     int count;
 public:
-    UnionFind(int n) : parent(n), rank(n, 1), count(n) {
+    UnionFind(int n) : parent(n), rank(n, 1), size(n, 1), count(n) {
         std::iota(parent.begin(), parent.end(), 0);
     }
 
@@ -27,11 +28,14 @@ public:
         }
         if ( rank[proot] < rank[qroot] ) {
             parent[proot] = qroot;
+            size[qroot] += size[proot];
         } else if ( rank[qroot] < rank[proot] ) {
             parent[qroot] = proot;
+            size[proot] += size[qroot];
         } else {
             parent[proot] = qroot;
             ++rank[qroot];
+            size[qroot] += size[proot];
         }
         --count;
     }
@@ -43,6 +47,8 @@ public:
     int Count() {
         return count;
     }
+
+    int Size(int p) {
+        return size[Find(p)];
+    }
 };
-
-
