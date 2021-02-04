e50f50c28586efe5822a387905232fe780a68254
tanzhuobo <tanzhuobo@gmail.com>
2021-02-04 09:48:17 +0800

    add solution2 to 1110

--- src/1110.cpp
+++ src/1110.cpp
@@ -33,3 +33,45 @@ public:
         return trees;
     }
 };
+
+
+
+class Solution2 {
+public:
+    vector<TreeNode*> delNodes(TreeNode* root, vector<int>& to_delete) {
+        stack<TreeNode*> stk;
+        vector<bool> del(1001, false);
+        vector<TreeNode*> res;
+        for ( int num : to_delete ) del[num] = true;
+        TreeNode* curr = root;
+        TreeNode* prev = nullptr;
+        if ( root != nullptr && !del[root->val] ) {
+            res.push_back(root);
+        }
+        while ( !stk.empty() || curr != nullptr ) {
+            while ( curr != nullptr ) {
+                stk.push(curr);
+                curr = curr->left;
+            }
+            curr = stk.top();
+            if ( curr->right == nullptr || curr->right == prev ) {
+                stk.pop();
+                if ( curr->left != nullptr && del[curr->left->val] ) {
+                    curr->left = nullptr;
+                }
+                if ( curr->right != nullptr && del[curr->right->val] ) {
+                    curr->right = nullptr;
+                }
+                if ( del[curr->val] ) {
+                    if ( curr->left != nullptr ) res.push_back(curr->left);
+                    if ( curr->right != nullptr ) res.push_back(curr->right);
+                }
+                prev = curr;
+                curr = nullptr;
+            } else {
+                curr = curr->right;
+            }
+        }
+        return res;
+    }
+};
