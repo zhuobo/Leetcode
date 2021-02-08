47267b23c0918b176bed76fd68a62c2360538ffe
tanzhuobo <tanzhuobo@gmail.com>
2021-02-08 16:45:40 +0800

    add solution 2 to 978

--- src/978.cpp
+++ src/978.cpp
@@ -53,3 +53,25 @@ public:
         return max_len;
     }
 };
+
+
+class Solution2 {
+public:
+    int maxTurbulenceSize(vector<int>& arr) {
+        int len = arr.size();
+        int up = 1, down = 1, max_len = 1;
+        for ( int i = 1; i < len; ++i ) {
+            if ( arr[i] == arr[i - 1] ) {
+                up = down = 1;
+            } else if ( arr[i] > arr[i - 1] ) {
+                up = down + 1;
+                down = 1;
+            } else {
+                down = up + 1;
+                up = 1;
+            }
+            max_len = max(max_len, max(up, down));
+        }
+        return max_len;
+    }
+};
