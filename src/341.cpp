1cd1805ca2d01820fb549dc89386e68df46e7ddb
tanzhuobo <tanzhuobo@gmail.com>
2021-03-23 09:11:29 +0800

    add 341

--- src/341.cpp
+++ src/341.cpp
@@ -11,33 +11,21 @@ using  std::stack;
 using std::vector;
 
 
-/**
- * // This is the interface that allows for creating nested lists.
- * // You should not implement it, or speculate about its implementation
- * class NestedInteger {
- *   public:
- *     // Return true if this NestedInteger holds a single integer, rather than a nested list.
- *     bool isInteger() const;
- *
- *     // Return the single integer that this NestedInteger holds, if it holds a single integer
- *     // The result is undefined if this NestedInteger holds a nested list
- *     int getInteger() const;
- *
- *     // Return the nested list that this NestedInteger holds, if it holds a nested list
- *     // The result is undefined if this NestedInteger holds a single integer
- *     const vector<NestedInteger> &getList() const;
- * };
- */
-class NestedIterator {
-public:
+class NestedInteger {
+  public:
     // 判断当前NestedInteger是否一个单一的整数
     bool isInteger() const;
     // 如果是单一的整数就返回这个整数,否则行为未定义 
     int getInteger() const;
     // 如果当前NestedInteger存储的是一系列NestedInteger,那么久返回这个list
-    const vector<NestedIterator> &getList() const;
+    const vector<NestedInteger> &getList() const;
+};
+// 只有当栈顶元素是一个整数的时候, 才能说明还有下一个元素.时间分散消耗在获取每一个数字上,不会集中消耗在构造函数的预处理,适合不一定要获取所有元素的场景.
 
-    NestedIterator(vector<NestedIterator> &list) {
+class NestedIterator {
+    stack<NestedInteger> stk;
+public:
+    NestedIterator(vector<NestedInteger> &list) {
         for ( auto it = list.rbegin(); it != list.rend(); ++it ) {
             stk.push(*it);
         }
@@ -63,52 +51,37 @@ public:
         }
         return false;
     }
-private:
-    stack<NestedIterator> stk;
 };
 
-// solution2
 
-/*
+// 使用一个vector预处理,预先把所有的整数都存储到vector中,这样时间主要消耗在构造函数的预处理上,后期取数据时间复杂度位O(1),适合一定要获取所有元素的场景.
 
-class NestedIterator {
-    vector<int> nums;
-    int         index;
+// class NestedIterator {
+    // vector<int> nums;
+    // int         index;
 
-    void ParseData(NestedInteger& item) {
-        if ( item.isInteger() ) {
-            nums.push_back(item.getInteger());
-        } else {
-            auto list = item.getList();
-            for ( auto iter = list.begin(); iter != list.end(); ++iter ) {
-                ParseData(*iter);
-            }
-        }
-    }
-public:
-    NestedIterator(vector<NestedInteger> &list) :index(0) {
-        for ( auto iter = list.begin(); iter != list.end(); ++iter ) {
-            ParseData(*iter);
-        }
-    }
+    // void ParseData(NestedInteger& item) {
+        // if ( item.isInteger() ) {
+            // nums.push_back(item.getInteger());
+        // } else {
+            // auto list = item.getList();
+            // for ( auto iter = list.begin(); iter != list.end(); ++iter ) {
+                // ParseData(*iter);
+            // }
+        // }
+    // }
+// public:
+    // NestedIterator(vector<NestedInteger> &list) :index(0) {
+        // for ( auto iter = list.begin(); iter != list.end(); ++iter ) {
+            // ParseData(*iter);
+        // }
+    // }
     
-    int next() {
-        return nums[index++];
-    }
+    // int next() {
+        // return nums[index++];
+    // }
     
-    bool hasNext() {
-        return index < nums.size();
-    }
-};
-*/
-
-/**
- * Your NestedIterator object will be instantiated and called as such:
- * NestedIterator i(nestedList);
- * while (i.hasNext()) cout << i.next();
- */
-
-
-
-
-
+    // bool hasNext() {
+        // return index < nums.size();
+    // }
+// };
