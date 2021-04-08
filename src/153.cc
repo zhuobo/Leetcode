f483f975711f65a98ebe5b0b892d3cc9962f777c
tanzhuobo <tanzhuobo@gmail.com>
2021-04-08 11:40:50 +0800

    153

---
+++ src/153.cc
@@ -0,0 +1,26 @@
+// Author:          Drinkwater
+// Email:           tanzhuobo@gmail.com
+// Last modified:   2021.04.08
+// Filename:        153.cc 
+// Descripton:       
+
+#include <vector>
+
+using std::vector;
+
+class Solution {
+public:
+    int findMin(vector<int>& nums) {
+        int left = 0;
+        int right = nums.size() - 1;
+        while ( left < right ) {
+            int mid = left + (right - left) / 2;
+            if ( nums[mid] < nums[right] ) {
+                right = mid;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return nums[left];
+    }
+};
